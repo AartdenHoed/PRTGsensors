e350e28591efb5ce66eb8a78c531cc65b8e17268
Aart den Hoed <adh.code@gmail.com>
2021-05-20 09:43:19 +0200

    Working code for future use

---
+++ CpuTemperature.ps1
@@ -0,0 +1,44 @@
+﻿# Needs admin privileges and the .NET OpenHardwareMonitorLib.dll
+ 
+#Requires -RunAsAdministrator
+ 
+CLS
+ 
+Add-Type -Path "D:\Software\OpenHardwareMonitor\OpenHardwareMonitorLib.dll"
+ 
+$Comp = New-Object -TypeName OpenHardwareMonitor.Hardware.Computer
+ 
+$Comp.Open()
+ 
+$Comp.CPUEnabled = $true
+ 
+$Comp.RAMEnabled = $true
+ 
+$Comp.MainboardEnabled = $true
+ 
+$Comp.FanControllerEnabled = $true
+ 
+$Comp.GPUEnabled = $true
+ 
+$Comp.HDDEnabled = $true
+ 
+ForEach ($HW in $Comp.Hardware) {
+ 
+    $HW.Update()    
+ 
+    If ( $hw.HardwareType -eq "CPU"){
+        $msg = $hw.HardwareType.ToString() + ' - ' + $hw.name.ToString()
+        Write-Host $msg
+        ForEach ($Sensor in $HW.Sensors) {
+ 
+        If ($Sensor.SensorType -eq "Temperature"){
+             
+            $Sensor.Name + ' - Temp : ' + $Sensor.Value.ToString() + ' C - Min. : ' + $Sensor.Min.ToString() + ' C - Max : ' + $Sensor.Max.ToString() + ' C'
+        }
+      }
+    }
+    
+    # $hw.Sensors
+    # $hw.SubHardware
+}
+$Comp.Close()
