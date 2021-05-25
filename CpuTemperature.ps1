40b60fdd43dc3929787661057dc150cd996a495b
Aart den Hoed <adh.code@gmail.com>
2021-05-25 21:36:57 +0200

    NEW PRTG sensor that measures CPU temperature

--- CpuTemperature.ps1
+++ CpuTemperature.ps1
@@ -1,44 +1,498 @@
-﻿# Needs admin privileges and the .NET OpenHardwareMonitorLib.dll
- 
-#Requires -RunAsAdministrator
- 
-CLS
- 
-Add-Type -Path "D:\Software\OpenHardwareMonitor\OpenHardwareMonitorLib.dll"
- 
-$Comp = New-Object -TypeName OpenHardwareMonitor.Hardware.Computer
- 
-$Comp.Open()
- 
-$Comp.CPUEnabled = $true
- 
-$Comp.RAMEnabled = $true
- 
-$Comp.MainboardEnabled = $true
- 
-$Comp.FanControllerEnabled = $true
- 
-$Comp.GPUEnabled = $true
- 
-$Comp.HDDEnabled = $true
- 
-ForEach ($HW in $Comp.Hardware) {
- 
-    $HW.Update()    
- 
-    If ( $hw.HardwareType -eq "CPU"){
-        $msg = $hw.HardwareType.ToString() + ' - ' + $hw.name.ToString()
-        Write-Host $msg
-        ForEach ($Sensor in $HW.Sensors) {
- 
-        If ($Sensor.SensorType -eq "Temperature"){
-             
-            $Sensor.Name + ' - Temp : ' + $Sensor.Value.ToString() + ' C - Min. : ' + $Sensor.Min.ToString() + ' C - Max : ' + $Sensor.Max.ToString() + ' C'
-        }
-      }
+﻿param (
+    [string]$LOGGING = "NO", 
+    [string]$myHost  = "????" ,
+    [int]$sensorid = 77 
+)
+# $LOGGING = 'YES'
+# $myHost = "hoesto"
+
+function Running-Elevated
+{
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { 
+        $adm = $true 
+    }      
+    else { 
+        $adm = $false 
     }
-    
-    # $hw.Sensors
-    # $hw.SubHardware
+    $MyAuth = [PSCustomObject] [ordered] @{ID = $id;
+                                           Principal = $p; 
+                                           Administrator = $adm}
+    return $MyAuth 
+ } 
+
+$myhost = $myhost.ToUpper()
+
+$ScriptVersion = " -- Version: 1.4"
+
+# COMMON coding
+CLS
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+$ErrorActionPreference = "Stop"
+
+$Node = " -- Node: " + $env:COMPUTERNAME
+$d = Get-Date
+$Datum = " -- Date: " + $d.ToShortDateString()
+$Tijd = " -- Time: " + $d.ToShortTimeString()
+
+$myname = $MyInvocation.MyCommand.Name
+$p = $myname.Split(".")
+$process = $p[0]
+$FullScriptName = $MyInvocation.MyCommand.Definition
+$mypath = $FullScriptName.Replace($MyName, "")
+
+$LocalInitVar = $mypath + "InitVar.PS1"
+& "$LocalInitVar"
+
+if (!$ADHC_InitSuccessfull) {
+    # Write-Warning "YES"
+    throw $ADHC_InitError
 }
-$Comp.Close()
+
+if ($LOGGING -eq "YES") {$log = $true} else {$log = $false}
+
+if ($log) {
+    $dir = $ADHC_OutputDirectory + $ADHC_PRTGlogs
+    # Write-Host $dir
+    if (!(Test-Path $dir)) {
+        New-Item -ItemType Directory -Force -Path $dir | Out-Null
+        # write-Host "Not"
+    }
+    if (!($sensorid -match "\d+")) {
+        $sensorid = 99999
+    }
+    $uniqueid = $sensorid.ToString("00000")
+    $logfile = $dir + $process + $uniqueid + ".log" 
+
+    $Scriptmsg = "Directory " + $mypath + " -- PowerShell script " + $MyName + $ScriptVersion + $Datum + $Tijd +$Node
+    Set-Content $logfile $Scriptmsg 
+
+    $thisdate = Get-Date
+    Add-Content $logfile "==> START $thisdate"
+}
+
+$scripterror = $false
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    # Function to write XML to log for PRTG
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xmlWriter.QuoteChar = '"'
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Host $StringWriter.ToString();
+    if ($log) {
+        Add-Content $logfile $StringWriter.ToString()
+    }
+}
+
+# END OF COMMON CODING
+# Get Node status
+
+$el = Running-Elevated
+
+if ($log) {
+    $u = $el.id.Name
+    $ia = $el.Principal.Identity.IsAuthenticated
+    Add-Content $logfile "==> Current user is $u (IsAuthenticated = $ia)" 
+    if (-not($el.Administrator)) {
+        Add-Content $logfile "==> Script NOT running as administrator"
+    }
+    else {
+       Add-Content $logfile "==> Script running as administrator"
+    }
+}
+
+if (!$scripterror) {
+    try {
+        if ($log) {
+            Add-Content $logfile "==> Get NODE status for $myhost"
+        }
+        $nstat = & $ADHC_NodeInfoScript "$myHost"  "$LOGGING" 
+
+    }
+    Catch {
+        if ($log) {
+            Add-Content $logfile "==> Getting NODE status failed for $myhost"
+        }
+        $scripterror = $true
+        $errortext = $error[0]
+        $scripterrormsg = "Getting NODE status failed for $myhost - $errortext"
+
+    }
+    finally {
+        if ($log) {
+            foreach ($m in $nstat.MessageList) {
+                $lvl = $m.Level
+                $msg = $m.Message
+                Add-COntent $logfile "($lvl) - $msg"
+
+            }
+        }
+    }
+
+}
+
+$CpuTempScript = $mypath + "CpuTemperature2.PS1"
+
+if (!$scripterror) {
+    try {
+        # CpuTemp info of machine
+        if ($log) {
+            Add-Content $logfile "==> Get CPU temperature info from machine $myHost"
+        }
+        $invokable = $true
+        if ($myHost -eq $ADHC_Computer.ToUpper()) {
+            $cmd = "& '" + "$CpuTempScript" + "'"
+            if ($log) {
+                Add-Content $logfile "==> Local script $cmd"
+            }
+            $CpuObject = invoke-expression -Command "$cmd" 
+            if ($CpuObject.MyStatus -ne "Ok") {
+                Throw $CpuObject.Message
+            }
+            $CpuTempInfo = $CpuObject.CPUlist
+        }
+        else {
+            try {
+                if ($log) {
+                    Add-Content $logfile "==> Remote script $CpuTempScript"
+                }
+                
+                $myjob = Invoke-Command -ComputerName $myhost `
+                    -FilePath $CpuTempscript  -Credential $ADHC_Credentials `
+                    -JobName CpuTempJob  -AsJob 
+                # write-host "Wait"
+                $myjob | Wait-Job -Timeout 150 | Out-Null
+                if ($myjob) { 
+                    $mystate = $myjob.state
+                } 
+                else {
+                    $mystate = "Unknown"
+                }
+                if ($log) {
+                    $mj = $myjob.Name
+                    Add-Content $logfile "==> Remote job $mj ended with status $mystate"
+                }
+                                
+                # Write-host $mystate
+                if ($mystate -eq "Completed") {
+                    # write-host "YES"
+                    $CpuObject = (Receive-Job -Name CpuTempJob)
+                    $CpuTempInfo = $CpuObject.CPUlist
+                    if ($log) {
+                        $m = "==> Called script ended with status " + $CpuObject.MyStatus + " --- Message: " + $CpuObject.Message
+                        Add-Content $logfile $m
+          
+                    } 
+                    if ($CpuObject.MyStatus -ne "Ok") {
+                        Throw $CpuObject.Message
+                    }
+                    
+                }
+                else {
+                    #write-host "NO"
+                    $invokable = $false
+                }
+                # write-host "Stop"
+                $myjob | Stop-Job | Out-Null
+                # write-host "Remove"
+                $myjob | Remove-Job | Out-null
+            }
+            catch {
+                # write-host "Catch"
+                $invokable = $false
+            }
+            finally {
+                
+                # Write-Host $nodeisup
+            }
+        }      
+        
+
+    }
+    catch {
+        if ($log) {
+            Add-Content $logfile "==> Getting CPU temperature info failed for $myHost"
+          
+        }
+        $scripterror = $true
+        $errortext = $error[0]
+        $scripterrormsg = "==> Getting CPU temperature info failed for $myHost - $errortext"
+    }
+}
+if (!$scripterror) {
+    try {
+        # 
+        if ($log) {
+            Add-Content $logfile "==> Process CPU temperature info"
+        }
+        # Init CpuTemp info file if not existent
+        $str = $ADHC_CpuTempInfo.Split("\")
+        $dir = $ADHC_OutputDirectory + $str[0] + "\" + $str[1]
+        New-Item -ItemType Directory -Force -Path $dir | Out-Null
+        $CpuTempInfoFile = $ADHC_OutputDirectory + $ADHC_CpuTempInfo.Replace($ADHC_Computer, $myHost)
+        $dr = Test-Path $CpuTempInfoFile
+        $timestamp = Get-Date
+        if (!$dr) {
+            $rec = "$MyHost|INIT|||||" + $timestamp.ToString("dd-MM-yyyy HH:mm:ss")
+            Set-Content $CpuTempInfoFile  $rec -force
+        }
+        
+        $cpulist = @()
+        if (!$invokable) {
+            # Node not invokable, get info from file
+
+
+            if ($log) {
+                Add-Content $logfile "==> Node is down, get info from dataset"
+            }
+            $CpuTemplines = Get-Content $CpuTempInfoFile
+            
+            
+            foreach ($entry in $CpuTemplines) {
+                $split = $entry.Split("|")
+                $CPUtype = $split[1]
+                if ($CPUtype -eq "INIT") { 
+                    # Status INIT, and no realtime info available
+                    $timestamp = [datetime]::ParseExact($split[6].Trim(),"dd-MM-yyyy HH:mm:ss",$null)
+
+                    $obj = [PSCustomObject] [ordered] @{Machine = $myhost; 
+                                                    CPUtype = $CPUtype;
+                                                    CPUname = "n/a";
+                                                    CpuTempCurrent = 0;
+                                                    CpuTempMin = 0;
+                                                    CpuTempMax = 0;
+                                                    Timestamp = $timestamp}                
+                }
+                else {
+                                
+                    $CpuName = $split[2]
+                    $CpuTempCurrent = $split[3]
+                    $CpuTempMin = $split[4]
+                    $CpuTempMax = $split[5]                              
+                    $timestamp = [datetime]::ParseExact($split[6].Trim(),"dd-MM-yyyy HH:mm:ss",$null)
+                    $obj = [PSCustomObject] [ordered] @{Machine = $myhost; 
+                                                        CPUtype = $CPUtype;
+                                                        CPUname = $CPUname;
+                                                        CpuTempCurrent = $CpuTempCurrent;
+                                                        CpuTempMin = $CpuTempMin;
+                                                        CpuTempMax = $CpuTempMax;
+                                                        Timestamp = $timestamp}
+                }
+                $cpulist += $obj
+           }
+        }
+
+        else {
+            # Node is UP, take real time info and write it tot dataset
+            if ($log) {
+                Add-Content $logfile "==> Node is up, get realtime info and write it to dataset"
+            }         
+            
+            $firstrec = $true
+            $timestamp = Get-Date
+            foreach ($cpu in $CpuTempInfo){
+                
+                $obj = [PSCustomObject] [ordered] @{Machine = $myhost; 
+                                                        CPUtype = $CPU.Type;
+                                                        CPUname = $CPU.Name;
+                                                        CpuTempCurrent = $Cpu.TempCurrent;
+                                                        CpuTempMin = $Cpu.TempMin;
+                                                        CpuTempMax = $Cpu.TempMax;
+                                                        Timestamp = $timestamp}
+                $cpulist += $obj
+                
+                $record = $myhost + "|" + $obj.CPUtype + "|" + $obj.CPUname + "|" + $obj.CpuTempCurrent + "|" + 
+                        $obj.CpuTempMin + "|" + $obj.CpuTempMax + "|" + $timestamp.ToString("dd-MM-yyyy HH:mm:ss")
+            
+                if ($firstrec) {
+                        Set-Content $CpuTempInfofile $record
+                        # Write-host "First"
+                        $firstrec = $false
+                }
+                else { 
+                    Add-Content $CpuTempInfofile $record
+                }
+            }
+                        
+        }
+
+    }   
+    catch {
+        if ($log) {
+            Add-Content $logfile "==> Processing CpuTemp info failed for $myHost"
+          
+        }
+        $scripterror = $true
+        $errortext = $error[0]
+        $scripterrormsg = "==> Processing CpuTemp info failed for $myHost - $errortext"
+    }
+}
+
+if ($log) {
+    Add-Content $logfile "==> Create XML"
+}
+
+[xml]$xmldoc = New-Object System.Xml.XmlDocument
+$decl = $xmldoc.CreateXmlDeclaration('1.0','Windows-1252',$null)
+
+[void]$xmldoc.AppendChild($decl)
+
+$PRTG = $xmldoc.CreateElement('PRTG')
+
+# Node status (PRIMARY Channel)
+$Result = $xmldoc.CreateElement('Result')
+$Channel = $xmldoc.CreateElement('Channel')
+$Value = $xmldoc.CreateElement('Value')
+$Unit = $xmldoc.CreateElement('Unit')
+$CustomUnit = $xmldoc.CreateElement('CustomUnit')
+$Mode = $xmldoc.CreateElement('Mode')
+$NotifyChanged = $xmldoc.CreateElement('NotifyChanged')
+$ValueLookup =  $xmldoc.CreateElement('ValueLookup')
+
+$Channel.InnerText = "Node status"
+$Unit.InnerText = "Custom"
+$Mode.Innertext = "Absolute"
+$ValueLookup.Innertext = 'NodeStatus'
+
+if ($invokable) {
+    $Value.Innertext = $nstat.StatusCode + 1
+    $livestat = $nstat.Status + ", Invokable"
+} 
+else { 
+   $Value.Innertext = $nstat.StatusCode
+   $livestat = $nstat.Status + ", Not Invokable"
+}
+
+
+[void]$Result.AppendChild($Channel)
+[void]$Result.AppendChild($Value)
+[void]$Result.AppendChild($Unit)
+[void]$Result.AppendChild($CustomUnit)
+[void]$Result.AppendChild($NotifyChanged)
+[void]$Result.AppendChild($ValueLookup)
+[void]$Result.AppendChild($Mode)
+    
+[void]$PRTG.AppendChild($Result)
+
+foreach ($item in $cpulist) {
+
+    $CpuType = $item.CPUtype
+
+    If ($CPUtype -eq "INIT") {
+        $CPUtype = "Not known yet (INIT phase)" 
+        break
+    }
+   
+    # Report Current Temp
+    $Result = $xmldoc.CreateElement('Result')
+    $Channel = $xmldoc.CreateElement('Channel')
+    $Value = $xmldoc.CreateElement('Value')    
+    $Unit = $xmldoc.CreateElement('Unit')
+    $Mode = $xmldoc.CreateElement('Mode')
+    $NotifyChanged = $xmldoc.CreateElement('NotifyChanged')
+    
+    $cname = $item.Machine + " " + $item.CpuName + " Current (C)"
+    $Channel.InnerText = $cname
+    $Unit.InnerText = "Temperature"
+    $Mode.Innertext = "Absolute"
+    $Value.Innertext = $item.CpuTempCurrent
+
+    [void]$Result.AppendChild($Channel)
+    [void]$Result.AppendChild($Value)
+    [void]$Result.AppendChild($Unit)
+   
+    [void]$Result.AppendChild($NotifyChanged)
+    
+    [void]$Result.AppendChild($Mode)
+    
+    [void]$PRTG.AppendChild($Result)
+
+    # Report Minimum Temp
+    $Result = $xmldoc.CreateElement('Result')
+    $Channel = $xmldoc.CreateElement('Channel')
+    $Value = $xmldoc.CreateElement('Value')    
+    $Unit = $xmldoc.CreateElement('Unit')
+    $Mode = $xmldoc.CreateElement('Mode')
+    $NotifyChanged = $xmldoc.CreateElement('NotifyChanged')
+    
+    $cname = $item.Machine + " " + $item.CpuName + " Minimum (C)"
+    $Channel.InnerText = $cname
+    $Unit.InnerText = "Temperature"
+    $Mode.Innertext = "Absolute"
+    $Value.Innertext = $item.CpuTempMin
+
+    [void]$Result.AppendChild($Channel)
+    [void]$Result.AppendChild($Value)
+    [void]$Result.AppendChild($Unit)
+    
+    [void]$Result.AppendChild($NotifyChanged)
+    
+    [void]$Result.AppendChild($Mode)
+    
+    [void]$PRTG.AppendChild($Result)
+
+    # Report Maximum Temp
+    $Result = $xmldoc.CreateElement('Result')
+    $Channel = $xmldoc.CreateElement('Channel')
+    $Value = $xmldoc.CreateElement('Value')    
+    $Unit = $xmldoc.CreateElement('Unit')
+    $Mode = $xmldoc.CreateElement('Mode')
+    $NotifyChanged = $xmldoc.CreateElement('NotifyChanged')
+    
+    $cname = $item.Machine + " " + $item.CpuName + " Maximum (C)"
+    $Channel.InnerText = $cname
+    $Unit.InnerText = "Temperature"
+    $Mode.Innertext = "Absolute"
+    $Value.Innertext = $item.CpuTempMax
+
+    [void]$Result.AppendChild($Channel)
+    [void]$Result.AppendChild($Value)
+    [void]$Result.AppendChild($Unit)
+  
+    [void]$Result.AppendChild($NotifyChanged)
+   
+    [void]$Result.AppendChild($Mode)
+    
+    [void]$PRTG.AppendChild($Result)
+    
+
+}
+
+# Add error block
+
+$ErrorValue = $xmldoc.CreateElement('Error')
+$ErrorText = $xmldoc.CreateElement('Text')
+
+if ($scripterror) {
+    $Errorvalue.InnerText = "1"
+    $ErrorText.InnerText = $scripterrormsg + " *** Scriptversion=$scriptversion *** "
+}
+else {
+    $ErrorValue.InnerText = "0"
+    
+    $message = "Machine $myhost (now $livestat) *** CPU type: $cputype *** Timestamp: $timestamp *** Script $scriptversion"
+    $ErrorText.InnerText = $message
+} 
+[void]$PRTG.AppendChild($ErrorValue)
+[void]$PRTG.AppendChild($ErrorText)
+    
+[void]$xmldoc.Appendchild($PRTG)
+
+if ($log) {
+    Add-Content $logfile "==> Write XML"
+}
+
+WriteXmlToScreen $xmldoc
+
+if ($log) {
+    $thisdate = Get-Date
+    Add-Content $logfile "==> END $thisdate"
+}
---
+++ CpuTemperature2.ps1
@@ -0,0 +1,105 @@
+﻿function Running-Elevated
+{
+   $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+   $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+   if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+   { Write-Output $true }      
+    else
+   { Write-Output $false }   
+}
+$status = "Ok"
+$CpuList = @()
+if (-not(Running-Elevated)) {
+    $msg =  "*** Script NOT running as administrator"
+    $status = "Error"
+}
+else {
+    $msg =  "*** Script running as administrator"
+} 
+
+if ($status -eq "Ok") {
+ 
+    Add-Type -Path "D:\Software\OpenHardwareMonitor\OpenHardwareMonitorLib.dll"
+ 
+    $Comp = New-Object -TypeName OpenHardwareMonitor.Hardware.Computer
+ 
+    $Comp.Open()
+ 
+    $Comp.CPUEnabled = $true
+ 
+    $Comp.RAMEnabled = $true
+ 
+    $Comp.MainboardEnabled = $true
+ 
+    $Comp.FanControllerEnabled = $true
+ 
+    $Comp.GPUEnabled = $true
+ 
+    $Comp.HDDEnabled = $true
+
+
+    $templist = @() 
+    $packagefound = $false
+    ForEach ($HW in $Comp.Hardware) {
+ 
+        $HW.Update()    
+ 
+        If ( $hw.HardwareType -eq "CPU"){
+            $type = $hw.name.ToString()
+        
+            ForEach ($Sensor in $HW.Sensors) {
+ 
+                If ($Sensor.SensorType -eq "Temperature"){
+                    $name =  $Sensor.Name 
+                    $tempcurrent = $Sensor.Value.ToString()
+                    $tempmin =$Sensor.Min.ToString()
+                    $tempmax = $Sensor.Max.ToString()
+            
+        
+                    $obj = [PSCustomObject] [ordered] @{Type = $type;
+                                                        Name = $name;
+                                                        TempCurrent = $tempcurrent;
+                                                        TempMin = $tempmin;
+                                                        TempMax = $tempmax}
+
+                    $templist += $obj
+                }
+
+                if ($name -eq "CPU Package") {
+                    $ptype = $type
+                    $pname = $name
+                    $ptempcurrent = $tempcurrent
+                    $ptempmin = $tempmin
+                    $ptempmax = $tempmax
+                    $packagefound = $true
+                }
+            } 
+                                            
+
+        }
+    
+    }
+    $Comp.Close()
+
+   
+    if ($packagefound) {
+        $obj = [PSCustomObject] [ordered] @{Type = $ptype;
+                                                    Name = $pname;
+                                                    TempCurrent = $ptempcurrent;
+                                                    TempMin = $ptempmin;
+                                                    TempMax = $ptempmax}
+        $CpuList += $obj
+    }
+
+    foreach ($c in $templist) {
+        if ($c.Name -ne "CPU Package") {
+            $CpuList += $c
+        
+        }
+    }
+}
+$Global:ReturnObject = [PSCustomObject] [ordered] @{Message = $msg;
+                                            MyStatus = $status;
+                                            CPUlist = $CpuList}
+
+Return $Global:ReturnObject
