89f968c6d2b41df5c0b2d72501dde58e574a9e16
ADHCode <adhc@hoedstorm.nl>
2025-06-28 14:56:29 +0200

    Removed logging. Just start job every 10 minutes

--- LocalCpuTemperature.ps1
+++ LocalCpuTemperature.ps1
@@ -1,4 +1,4 @@
-﻿$Version = " -- Version: 1.5"
+﻿$Version = " -- Version: 2.0"
 
 # COMMON coding
 CLS
@@ -78,10 +78,6 @@ if ($Initobj.AbEnd) {
    
 # END OF COMMON CODING
 
-$gp = Get-Process -id $pid 
-$ProcessID = $gp.Id
-$ProcessName = $gp.Name      
-
 # Init reporting file
 $dir = $ADHC_TempDirectory + $ADHC_LocalCpuTemperature.Directory
 New-Item -ItemType Directory -Force -Path $dir | Out-Null
@@ -94,8 +90,6 @@ $p = $myname.Split(".")
 $process = $p[0]
 $jobstatus = $ADHC_OutputDirectory + $ADHC_Jobstatus + $ADHC_Computer + "_" + $Process + ".jst" 
 
-$errorcount = 0
-$loop = 0
 $myname = $MyInvocation.MyCommand.Name
     
 $FullScriptName = $MyInvocation.MyCommand.Definition
@@ -103,91 +97,77 @@ $mypath = $FullScriptName.Replace($MyName, "")
 
 $sensorscript = $mypath + "CpuTemperature.ps1"
 
-do {
+try {
     $d = Get-Date
     $Datum = " -- Date: " + $d.ToString("dd-MM-yyyy")
     $Tijd = " -- Time: " + $d.ToString("HH:mm:ss")
-    $loop = $loop + 1
+   
     $Scriptmsg = "*** STARTED *** " + $mypath + " -- PowerShell script " + $MyName + $Version + $Datum + $Tijd +$Node
     Write-Information $Scriptmsg 
     Set-Content $Tempfile $Scriptmsg -force
-    if ($loop -le 1) {
-        foreach ($entry in $InitObj.MessageList){
-            Report $entry.Level $entry.Message $StatusObj $Tempfile
+    foreach ($entry in $InitObj.MessageList){
+        Report $entry.Level $entry.Message $StatusObj $Tempfile
+    }
+
+    Report "I" "Started... sensor script = $sensorscript" $StatusObj $Tempfile
+    $Sensor = & "$Sensorscript" YES $ADHC_Computer 9000
+    Report "I" "Ended... sensor script = $sensorscript" $StatusObj $Tempfile
+
+}
+catch {
+    Report "E" "Error !!!" $StatusObj $Tempfile
+    $errorcount += 1
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    $Dump = $_.Exception.ToSTring()
+    Report "E" "Message = $ErrorMessage" $StatusObj $Tempfile
+    Report "E" "Failed Item = $Faileditem" $StatusObj $Tempfile
+    Report "E" "Dump = $Dump" $StatusObj $Tempfile
+} 
+finally {
+       
+    if  ($StatusObj.scripterror) {
+        $dt = Get-Date
+        $jobline = $ADHC_Computer + "|" + $process + "|" + "9" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
+        Set-Content $jobstatus $jobline
+       
+        Add-Content $jobstatus "Failed item = $FailedItem"
+        Add-Content $jobstatus "Errormessage = $ErrorMessage"
+        Add-Content $jobstatus "Dump info = $dump"
+
+        Report "E" "Failed item = $FailedItem" $StatusObj $Tempfile
+        Report "E" "Errormessage = $ErrorMessage" $StatusObj $Tempfile
+        Report "E" "Dump info = $dump" $StatusObj $Tempfile
         }
+    else {
+        Report "I" ">>> Sensor ended normally $Datum $Tijd" $StatusObj $Tempfile
+        Report "N" " " $StatusObj $Tempfile
+   
+        $dt = Get-Date
+        $jobline = $ADHC_Computer + "|" + $process + "|" + "0" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
+        Set-Content $jobstatus $jobline
+
     }
-
-    Report "I" "Process name = $ProcessName, proces ID = $ProcessID" $StatusObj $Tempfile
-
-    Report "I"  "Iteration number $loop ($errorcount errors until now)" $StatusObj $Tempfile
+    
+    Report "N" " " $StatusObj $Tempfile
+    try { #  copy temp file
         
-    try {
-
-        Report "I" "Started... sensor script = $sensorscript" $StatusObj $Tempfile
-        $Sensor = & "$Sensorscript" YES $ADHC_Computer 9000
-        Report "I" "Ended... sensor script = $sensorscript" $StatusObj $Tempfile
-
+        $deffile = $ADHC_OutputDirectory + $ADHC_LocalCpuTemperature.Directory + $ADHC_LocalCpuTemperature.Name 
+        $CopMov = & $ADHC_CopyMoveScript $TempFile $deffile "COPY" "REPLACE" $TempFile  
+            
     }
-    catch {
-        Report "E" "Error !!!" $StatusObj $Tempfile
-        $errorcount += 1
+    Catch {
         $ErrorMessage = $_.Exception.Message
         $FailedItem = $_.Exception.ItemName
         $Dump = $_.Exception.ToSTring()
-        Report "E" "Message = $ErrorMessage" $StatusObj $Tempfile
-        Report "E" "Failed Item = $Faileditem" $StatusObj $Tempfile
-        Report "E" "Dump = $Dump" $StatusObj $Tempfile
-    } 
-    finally {
-       
-        if  ($StatusObj.scripterror) {
-            $dt = Get-Date
-            $jobline = $ADHC_Computer + "|" + $process + "|" + "9" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
-            Set-Content $jobstatus $jobline
-       
-            Add-Content $jobstatus "Failed item = $FailedItem"
-            Add-Content $jobstatus "Errormessage = $ErrorMessage"
-            Add-Content $jobstatus "Dump info = $dump"
-
-            Report "E" "Failed item = $FailedItem" $StatusObj $Tempfile
-            Report "E" "Errormessage = $ErrorMessage" $StatusObj $Tempfile
-            Report "E" "Dump info = $dump" $StatusObj $Tempfile
-            }
-        else {
-            Report "I" ">>> Iteration ended normally $Datum $Tijd" $StatusObj $Tempfile
-            Report "N" " " $StatusObj $Tempfile
-   
-            $dt = Get-Date
-            $jobline = $ADHC_Computer + "|" + $process + "|" + "0" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
-            Set-Content $jobstatus $jobline
-
-        }
-        Report "I" "Wait 300 seconds..." $StatusObj $Tempfile
-        Report "N" " " $StatusObj $Tempfile
-        try { #  copy temp file
+        $dt = Get-Date
+        $jobline = $ADHC_Computer + "|" + $process + "|" + "9" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
+        Set-Content $jobstatus $jobline
+        Add-Content $jobstatus "Failed item = $FailedItem"
+        Add-Content $jobstatus "Errormessage = $ErrorMessage"
+        Add-Content $jobstatus "Dump info = $Dump"
         
-            $deffile = $ADHC_OutputDirectory + $ADHC_LocalCpuTemperature.Directory + $ADHC_LocalCpuTemperature.Name 
-            $CopMov = & $ADHC_CopyMoveScript $TempFile $deffile "COPY" "REPLACE" $TempFile  
-            
-        }
-        Catch {
-            $ErrorMessage = $_.Exception.Message
-            $FailedItem = $_.Exception.ItemName
-            $Dump = $_.Exception.ToSTring()
-            $dt = Get-Date
-            $jobline = $ADHC_Computer + "|" + $process + "|" + "9" + "|" + $version + "|" + $dt.ToString("dd-MM-yyyy HH:mm:ss")
-            Set-Content $jobstatus $jobline
-            Add-Content $jobstatus "Failed item = $FailedItem"
-            Add-Content $jobstatus "Errormessage = $ErrorMessage"
-            Add-Content $jobstatus "Dump info = $Dump"
-            $errorcount += 1    
+    }    
+    
+}
 
-        }    
-        
-        
-        Start-Sleep -s 300
-    }
-
-} Until ($errorcount -gt 10)
-
-Report "I" "Ended with error count = $errorcount" $StatusObj $Tempfile
